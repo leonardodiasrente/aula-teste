dfd4f41b800160ae9e110ff7caef7b8bf8cf5d58
leonardodiasrente <94146305+leonardodiasrente@users.noreply.github.com>
2021-11-11 19:55:44 -0300

    p5.js

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,17 @@
+var ball;
+
+function setup() {
+  createCanvas(400,400);
+  ball =createSprite(200,200,20,20);
+}
+
+function draw() 
+{
+  //as cores vão de 250 para branco e 0 para preto
+  background(250);
+drawnSprites();
+}
+
+
+
+
